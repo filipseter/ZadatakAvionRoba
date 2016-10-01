5cc1f232b9fd4a650017a025a14659019573c4b4
Filip <e@domain.com>
2016-10-01 20:15:19 +0200

    fixed #6 Uradjen zadatk brisanje vrednosti

--- src/main/java/zadaci/Zadatak4BrisanjeVrednosti.java
+++ src/main/java/zadaci/Zadatak4BrisanjeVrednosti.java
@@ -4,9 +4,13 @@ package zadaci;
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.stmt.PreparedQuery;
+import com.j256.ormlite.stmt.QueryBuilder;
 import com.j256.ormlite.support.ConnectionSource;
 import model.Avion;
 import model.Roba;
+
+import java.util.List;
 
 public class Zadatak4BrisanjeVrednosti {
 
@@ -22,6 +26,25 @@ public class Zadatak4BrisanjeVrednosti {
             cs = new JdbcConnectionSource(Avion.DATABASE_URL);
             avionDao = DaoManager.createDao( cs, Avion.class );
             robaDao = DaoManager.createDao( cs, Roba.class );
+
+            List<Roba> robaList = robaDao.queryForAll();
+            Avion.print_break();
+            for (Roba roba : robaList )
+                System.out.println( roba );
+            Avion.print_break();
+
+            QueryBuilder<Roba, Integer> robaQB = robaDao.queryBuilder();
+            robaQB.where().like(Roba.POLJE_OPIS, "%Voda%");
+            PreparedQuery<Roba> robaPQ = robaQB.prepare();
+            robaList = robaDao.query( robaPQ );
+            Roba robaZaBrisanje = robaList.get(0);
+            robaDao.delete( robaZaBrisanje );
+
+            robaList = robaDao.queryForAll();
+            Avion.print_break();
+            for (Roba roba : robaList )
+                System.out.println( roba );
+            Avion.print_break();
 
 
         } finally {
