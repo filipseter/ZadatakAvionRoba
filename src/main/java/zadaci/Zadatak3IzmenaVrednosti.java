715cd3768647e2b1626f4775211cf55610ec9813
Filip <e@domain.com>
2016-10-01 20:09:40 +0200

    fixed #5 Uradjen zadatak izmena vrednosti

--- src/main/java/zadaci/Zadatak3IzmenaVrednosti.java
+++ src/main/java/zadaci/Zadatak3IzmenaVrednosti.java
@@ -4,9 +4,13 @@ package zadaci;
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.stmt.PreparedQuery;
+import com.j256.ormlite.stmt.QueryBuilder;
 import com.j256.ormlite.support.ConnectionSource;
 import model.Avion;
 import model.Roba;
+
+import java.util.List;
 
 public class Zadatak3IzmenaVrednosti {
 
@@ -23,6 +27,23 @@ public class Zadatak3IzmenaVrednosti {
             avionDao = DaoManager.createDao( cs, Avion.class );
             robaDao = DaoManager.createDao( cs, Roba.class );
 
+            QueryBuilder<Roba, Integer> robaQB = robaDao.queryBuilder();
+            robaQB.where().like(Roba.POLJE_OPIS, "%Plasticna stolica%");
+            PreparedQuery<Roba> robaPQ = robaQB.prepare();
+            List<Roba> robaList = robaDao.query( robaPQ );
+            Avion.print_break();
+            for (Roba roba : robaList )
+                System.out.println( roba );
+            Avion.print_break();
+            Roba robaZaIzmenu = robaList.get(0);
+            robaZaIzmenu.setOpis("Drvena stolica");
+
+            robaDao.update( robaZaIzmenu );
+
+            Avion.print_break();
+            for (Roba roba : robaList )
+                System.out.println( roba );
+            Avion.print_break();
 
         } finally {
             if ( cs != null )
